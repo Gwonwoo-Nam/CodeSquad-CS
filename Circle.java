fe3aba0f029ca980d6b5c96528a8d8722349ffeb
Gwonwoo Nam <ngw7617@naver.com>
2022-12-05 12:17:40 +0900

    feat(solar_system_stage_1):create Circle Interface
    - size 상태와 draw 메서드를 가진 Circle 인터페이스 구현

---
+++ Circle.java
@@ -0,0 +1,7 @@
+
+import java.util.List;
+
+public interface Circle {
+    List<Character> draw(int size);
+
+}
---
+++ SolarCircle.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+
+public class SolarCircle implements Circle {
+    private final int size = 0;
+
+    @Override
+    public List<Character> draw(int size) {
+        return null;
+    }
+
+
+}
