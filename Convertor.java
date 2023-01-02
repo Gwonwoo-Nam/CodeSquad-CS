6d58fe25763da22aec6f2e00fa4a1d849b1d42c7
Gwonwoo-Nam <ngw7617@naver.com>
2023-01-02 13:42:16 +0900

    refactor(convertor):메인 함수 분리 및 출력 함수 convertor 내부로 리팩터

--- Convertor.java
+++ Convertor.java
@@ -46,24 +46,12 @@ class Convertor {
     private int power(int index) {
        return (int)Math.pow(2,index);
     }
-}
 
-class TestApplication {
-    static int dec2binTest1 = 10;
-    static int dec2binTest2 = 173;
-    static boolean[] dec2binTestAnswer1 = {false, true, false, true};
-    static boolean[] dec2binTestAnswer2 = {true, false, true, true, false, true, false, true};
-    static boolean[] bin2decTest1 = {false, true, true, true};
-    static boolean[] bin2decTest2 = {true, true, true, true, false, true, false, true};
-    static int bin2decTestAnswer1 = 14;
-    static int bin2decTestAnswer2 = 175;
+    public void printBin(boolean[] bin) {
+        System.out.println(Arrays.toString(bin));
+    }
 
-    public static void main(String[] args) {
-        Convertor convertor = new Convertor();
-
-        System.out.println(Arrays.toString(convertor.dec2bin(dec2binTest1)) + " 정답 : " + Arrays.toString(dec2binTestAnswer1));
-        System.out.println(Arrays.toString(convertor.dec2bin(dec2binTest2)) + " 정답 : " + Arrays.toString(dec2binTestAnswer2));
-        System.out.println(convertor.bin2dec(bin2decTest1) + " 정답 : " + bin2decTestAnswer1);
-        System.out.println(convertor.bin2dec(bin2decTest2) + " 정답 : " + bin2decTestAnswer2);
+    public void printDec(int dec) {
+        System.out.println(dec);
     }
 }
---
+++ TestApplication.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class TestApplication {
+    static int dec2binTest1 = 10;
+    static int dec2binTest2 = 173;
+    static boolean[] dec2binTestAnswer1 = {false, true, false, true};
+    static boolean[] dec2binTestAnswer2 = {true, false, true, true, false, true, false, true};
+    static boolean[] bin2decTest1 = {false, true, true, true};
+    static boolean[] bin2decTest2 = {true, true, true, true, false, true, false, true};
+    static int bin2decTestAnswer1 = 14;
+    static int bin2decTestAnswer2 = 175;
+
+    public static void main(String[] args) {
+        Convertor convertor = new Convertor();
+
+        boolean[] dec2binMyAnswer1 = convertor.dec2bin(dec2binTest1);
+        boolean[] dec2binMyAnswer2 = convertor.dec2bin(dec2binTest2);
+        convertor.printBin(dec2binMyAnswer1);
+        convertor.printBin(dec2binTestAnswer1);
+        convertor.printBin(dec2binMyAnswer2);
+        convertor.printBin(dec2binTestAnswer2);
+
+        int bin2decMyAnswer1 = convertor.bin2dec(bin2decTest1);
+        int bin2decMyAnswer2 = convertor.bin2dec(bin2decTest2);
+        convertor.printDec(bin2decMyAnswer1);
+        convertor.printDec(bin2decTestAnswer1);
+        convertor.printDec(bin2decMyAnswer2);
+        convertor.printDec(bin2decTestAnswer2);
+    }
+
+
+}
