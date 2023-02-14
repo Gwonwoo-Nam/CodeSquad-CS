bc1dcfdf470b2ea1f21e225ed4d4ce0cd68b1db7
Gwonwoo-Nam <ngw7617@naver.com>
2023-02-14 13:58:17 +0900

    feat(CS12):Cache 동작 추가

--- Cache.java
+++ Cache.java
@@ -1,2 +1,18 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Cache {
+    /**
+     * LFU 알고리즘 - 참조 횟수가 가장 적은 것부터 poll
+     */
+    private PriorityQueue<Response> cacheQueue = new PriorityQueue();
+    private int maxCapacity = 10;
+
+    public void addCache(Response response) {
+        if (cacheQueue.size() >= maxCapacity) {
+            cacheQueue.add(response);
+        }
+    }
+
+    public void
 }
--- Response.java
+++ Response.java
@@ -1,8 +1,9 @@
 import java.net.http.HttpResponse;
 
-public class Response {
+public class Response implements Comparable {
     private HttpResponse<String> response;
     private long time;
+    private int referenced = 1;
 
     public Response(HttpResponse<String> response) {
         this.response = response;
@@ -19,4 +20,20 @@ public class Response {
     public void setTime(long time) {
         this.time = time;
     }
+
+    public void isReferenced() {
+        this.referenced++;
+    }
+
+    public int getReferenced() {
+        return referenced;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        //참조 횟수가 적은 것이 우선순위가 높다.(먼저 큐에서 빠짐)
+        Response target = (Response)o;
+        return target.getReferenced() < this.getReferenced() ? 1 : -1;
+    }
+
 }
